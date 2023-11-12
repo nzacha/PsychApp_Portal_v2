0b9cbf4fb340d5bcbdc2c02540ac9b9b1ea78581
nzacha <zicolas3@gmail.com>
2023-11-12 19:03:44 +0200

    env change

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -13,10 +13,14 @@ const Prodution: Environment = {
 };
 
 const Development: Environment = {
-    API_BASE_URL: 'http://localhost:5050',
-    CHAT_BASE_URL: 'http://localhost:5051',
-    APP_NAME: 'PsychApp Development',
-    STATIC_FILES_URL: 'http://localhost:5050/static',
+    API_BASE_URL: 'http://45.134.226.253:5050',
+    CHAT_BASE_URL: 'http://45.134.226.253:5051',
+    APP_NAME: 'PsychApp',
+    STATIC_FILES_URL: 'http://45.134.226.253:/static',
+    // API_BASE_URL: 'http://localhost:5050',
+    // CHAT_BASE_URL: 'http://localhost:5051',
+    // APP_NAME: 'PsychApp Development',
+    // STATIC_FILES_URL: 'http://localhost:5050/static',
 };
 
 export default process.env.NODE_ENV === 'production' ? Prodution : Development;
